549928c5ff5466a76acbfe7579de0bda6430b822
leahnp613 <leahnagarpowers@gmail.com>
2023-07-11 15:24:07 -0400

    finished data to the dom

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,29 +1,33 @@
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener("DOMContentLoaded", async () => {
+  const url = "http://localhost:8000/api/conferences";
 
-    const url = 'http://localhost:8000/api/conferences';
+  try {
+    const response = await fetch(url);
 
-    try{
-        const response = await fetch(url);
-        
-        if (!response.ok) {
-          //Figure out what to do when the response is bad
-        } else{
-            const data = await response.json();
+    if (!response.ok) {
+      //Figure out what to do when the response is bad
+    } else {
+      const data = await response.json();
 
-            const conference = data.conferences[1];
-            const nameTag = document.querySelector('.card-title');
-            nameTag.innerHTML = conference.name;
-            console.log(conference);
+      const conference = data.conferences[1];
+      const nameTag = document.querySelector('.card-title');
+      nameTag.innerHTML = conference.name;
+    //   console.log(conference);
 
-            const detailUrl = 'http://localhost:8000${conference.href}';
-            const detailResponse = await fetch (detailUrl);
-            if (detailResponse.ok) {
-                const details = await detailResponse.json();
-                console.log(details);
-            }
-        }
-    }catch (e){
-    //Figure out what to do if an error is raised
+      
+
+      const detailUrl =`http://localhost:8000${conference.href}`;
+      const detailResponse = await fetch(detailUrl);
+      if (detailResponse.ok) {
+        const details = await detailResponse.json();
+        const description = details.conference.description;
+        const descriptor = document.querySelector('.card-text');
+        descriptor.innerHTML = details.conference.description;
+        console.log(description)
+         console.log(details);
+      }
     }
-    
+  } catch (e) {
+    //Figure out what to do if an error is raised
+  }
 });

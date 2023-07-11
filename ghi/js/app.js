6292ce22d4c9a943a37dfd6405fcb31c70e53cb0
leahnp613 <leahnagarpowers@gmail.com>
2023-07-11 12:40:52 -0400

    data to dom

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -6,12 +6,24 @@ window.addEventListener('DOMContentLoaded', async () => {
         const response = await fetch(url);
         
         if (!response.ok) {
-            console.error("Fix this.")
+          //Figure out what to do when the response is bad
         } else{
             const data = await response.json();
+
+            const conference = data.conferences[1];
+            const nameTag = document.querySelector('.card-title');
+            nameTag.innerHTML = conference.name;
+            console.log(conference);
+
+            const detailUrl = 'http://localhost:8000${conference.href}';
+            const detailResponse = await fetch (detailUrl);
+            if (detailResponse.ok) {
+                const details = await detailResponse.json();
+                console.log(details);
+            }
         }
     }catch (e){
-    console.error("Check your JS")
+    //Figure out what to do if an error is raised
     }
     
 });

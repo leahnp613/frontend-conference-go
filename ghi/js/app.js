9d74eec86f13d5ddf39b20c633292598d2843523
leahnp613 <leahnagarpowers@gmail.com>
2023-07-11 12:17:04 -0400

    first commit

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,4 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
 
+    const url = 'http://localhost:8000/api/conferences';
 
+    try{
+        const response = await fetch(url);
+        
+        if (!response.ok) {
+            console.error("Fix this.")
+        } else{
+            const data = await response.json();
+        }
+    }catch (e){
+    console.error("Check your JS")
+    }
+    
 });

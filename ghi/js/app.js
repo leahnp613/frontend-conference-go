00d95c854e0f8cbd4c223cf808559fc5ae13684c
leahnp613 <leahnagarpowers@gmail.com>
2023-07-12 12:01:28 -0400

    single column

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,3 +1,15 @@
+function createCard(name, description, pictureUrl) {
+    return `
+      <div class="card">
+        <img src="${pictureUrl}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">${name}</h5>
+          <p class="card-text">${description}</p>
+        </div>
+      </div>
+    `;
+  }
+
 window.addEventListener('DOMContentLoaded', async () => {
 
     const url = 'http://localhost:8000/api/conferences/';
@@ -10,22 +22,28 @@ window.addEventListener('DOMContentLoaded', async () => {
       } else {
         const data = await response.json();
   
-        const conference = data.conferences[0];
-        const nameTag = document.querySelector('.card-title');
-        nameTag.innerHTML = conference.name;
-        console.log(conference);
+        // const conference = data.conferences[0];
+        // const nameTag = document.querySelector('.card-title');
+        // nameTag.innerHTML = conference.name;
+        // console.log(conference);
 
+        for (let conference of data.conferences){
         const detailUrl = `http://localhost:8000${conference.href}`;
-        console.log(detailUrl);
         const detailResponse = await fetch(detailUrl);
         if (detailResponse.ok) {
             const details = await detailResponse.json();
-        const imageTag = document.querySelector('.card-img-top');
-            imageTag.src = details.conference.location.picture_url
+            const title = details.conference.title;
+            const description = details.conference.description;
+            const pictureUrl = details.conference.location.picture_url;
+            const html = createCard(title, description, pictureUrl);
+            const column = document.querySelector('.col');
+            column.innerHTML += html;
+        // const imageTag = document.querySelector('.card-img-top');
+        //     imageTag.src = details.conference.location.picture_url
             console.log(details);
             console.log("hello");
         }
-  
+    }
       }
     } catch (e) {
       // Figure out what to do if an error is raised

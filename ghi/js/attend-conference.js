343308b9174f9a2234cc01a5ebff66feeb41d613
leahnp613 <leahnagarpowers@gmail.com>
2023-07-13 15:08:39 -0400

    finished adding the attendee

--- ghi/js/attend-conference.js
+++ ghi/js/attend-conference.js
@@ -1,24 +1,41 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const selectTag = document.getElementById('conference');
-  
     const url = 'http://localhost:8000/api/conferences/';
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
-  
       for (let conference of data.conferences) {
         const option = document.createElement('option');
         option.value = conference.href;
         option.innerHTML = conference.name;
         selectTag.appendChild(option);
       }
-
-      const loadingIcon = document.getElementById('loading-conference-spinner');
-      loadingIcon.classList.add('d-none');
+      const spinner = document.getElementById('loading-conference-spinner');
+      spinner.classList.add('d-none');
       selectTag.classList.remove('d-none');
-
-
-
     }
-  
-  });
+    const formTag = document.getElementById('create-attendee-form');
+    formTag.addEventListener('submit', async event => {
+        event.preventDefault();
+        const formData = new FormData(formTag)
+        const json = JSON.stringify(Object.fromEntries(formData))
+        console.log(json)
+        const attendeeUrl = 'http://localhost:8001/api/attendees/';
+        const fetchConfig ={
+            method:"post",
+            body:json,
+            header:{
+                'Content-Type': 'application/json',
+            },
+        };
+        const response = await fetch(attendeeUrl, fetchConfig);
+        const sucess = document.getElementById('success-message');
+        sucess.classList.remove('d-none');
+        formTag.classList.add('d-none');
+        if (response.ok){
+            formTag.reset();
+            const newAttendee = await response.json();
+            console.log(newAttendee)
+        }
+    })
+})

3860fdf9af54eb352a7cfd9cb5df5ee6a10504b4
leahnp613 <leahnagarpowers@gmail.com>
2023-07-13 10:31:42 -0400

    attend-conference.html

--- ghi/js/app.js
+++ ghi/js/app.js
@@ -1,5 +1,5 @@
 function createCard(name, description, pictureUrl) {
-    return `
+  return `
       <div class="card">
         <img src="${pictureUrl}" class="card-img-top">
         <div class="card-body">
@@ -8,45 +8,43 @@ function createCard(name, description, pictureUrl) {
         </div>
       </div>
     `;
-  }
+}
 
-window.addEventListener('DOMContentLoaded', async () => {
+window.addEventListener("DOMContentLoaded", async () => {
+  const url = "http://localhost:8000/api/conferences/";
 
-    const url = 'http://localhost:8000/api/conferences/';
-  
-    try {
-      const response = await fetch(url);
-  
-      if (!response.ok) {
-        // Figure out what to do when the response is bad
-      } else {
-        const data = await response.json();
-  
-        // const conference = data.conferences[0];
-        // const nameTag = document.querySelector('.card-title');
-        // nameTag.innerHTML = conference.name;
-        // console.log(conference);
+  try {
+    const response = await fetch(url);
 
-        for (let conference of data.conferences){
+    if (!response.ok) {
+      // Figure out what to do when the response is bad
+    } else {
+      const data = await response.json();
+
+      // const conference = data.conferences[0];
+      // const nameTag = document.querySelector('.card-title');
+      // nameTag.innerHTML = conference.name;
+      // console.log(conference);
+
+      for (let conference of data.conferences) {
         const detailUrl = `http://localhost:8000${conference.href}`;
         const detailResponse = await fetch(detailUrl);
         if (detailResponse.ok) {
-            const details = await detailResponse.json();
-            const title = details.conference.title;
-            const description = details.conference.description;
-            const pictureUrl = details.conference.location.picture_url;
-            const html = createCard(title, description, pictureUrl);
-            const column = document.querySelector('.col');
-            column.innerHTML += html;
-        // const imageTag = document.querySelector('.card-img-top');
-        //     imageTag.src = details.conference.location.picture_url
-            console.log(details);
-            console.log("hello");
+          const details = await detailResponse.json();
+          const title = details.conference.title;
+          const description = details.conference.description;
+          const pictureUrl = details.conference.location.picture_url;
+          const html = createCard(title, description, pictureUrl);
+          const column = document.querySelector(".col");
+          column.innerHTML += html;
+          // const imageTag = document.querySelector('.card-img-top');
+          //     imageTag.src = details.conference.location.picture_url
+          console.log(details);
+          console.log("hello");
         }
-    }
       }
-    } catch (e) {
-      // Figure out what to do if an error is raised
     }
-  
-  });
+  } catch (e) {
+    // Figure out what to do if an error is raised
+  }
+});
--- ghi/js/new-location.js
+++ ghi/js/new-location.js
@@ -1,25 +1,52 @@
-window.addEventListener('DOMContentLoaded', async () => {
-
-    const url = 'http://localhost:8000/api/states/';
-  
-    const response = await fetch(url);
-  
-    if (response.ok) {
-      const data = await response.json();
-  
-      const selectTag = document.getElementById('state');
-      for (let state of data.states) {
-        // Create an 'option' element
-        const option = document.createElement('option');
-        // Set the '.value' property of the option element to the
-        // state's abbreviation
-        option.value = state.abbreviation;
-        // Set the '.innerHTML' property of the option element to
-        // the state's name
-        option.innerHTML = state.name;
-        // Append the option element as a child of the select tag
-        selectTag.appendChild(option);
+window.addEventListener("DOMContentLoaded", async () => {
+    const url = "http://localhost:8000/api/states/";
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        const states = data.states;
+        const selectTag = document.getElementById("state");
+        for (let state of states) {
+          const option = document.createElement("option");
+          option.value = state.abbreviation;
+          option.innerHTML = state.name;
+          selectTag.appendChild(option);
+        }
+      }
+    } catch (e) {
+      return `<div class="alert alert-danger" role="alert">
+          Something went wrong!
+         </div>`;
     }
-
-    }
+    //   user creates a location
+    const formTag = document.getElementById("create-location-form");
+    formTag.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      const formData = new FormData(formTag);
+      const json = JSON.stringify(Object.fromEntries(formData));
+      const locationUrl = "http://localhost:8000/api/locations/";
+      const fetchConfig = {
+        method: "POST",
+        body: json,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      try {
+        const response = await fetch(locationUrl, fetchConfig);
+        if (response.ok) {
+          formTag.reset();
+          const newLocation = await response.json();
+        }
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    });
   });
+
+  
+  
+  
+  
+  
+  

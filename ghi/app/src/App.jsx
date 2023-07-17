c589662a909486823ee6027b7866723708da6764
leahnp613 <leahnagarpowers@gmail.com>
2023-07-17 12:24:42 -0400

    showing a form

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -1,4 +1,5 @@
 import Nav from './Nav';
+import AttendeesList from './AttendeesList';
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -6,30 +7,12 @@ function App(props) {
   }
   return (
     <>
-    <Nav />
-    <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{ attendee.name }</td>
-                <td>{ attendee.conference }</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+      <Nav />
+      <div className="container">
+        <AttendeesList attendees={props.attendees} />
+      </div>
     </>
   );
 }
 
 export default App;
-

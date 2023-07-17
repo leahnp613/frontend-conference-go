3c2802e60a7e3aef9f31badff7790c24b5c2ee4a
leahnp613 <leahnagarpowers@gmail.com>
2023-07-17 11:33:54 -0400

    changed api_views.py to fix list attendees

--- attendees_microservice/attendees/api_views.py
+++ attendees_microservice/attendees/api_views.py
@@ -57,28 +57,32 @@ def api_list_attendees(request, conference_vo_id=None):
         ]
     }
     """
-    if conference_vo_id is not None:
+    if request.method == "GET":
         attendees = Attendee.objects.filter(conference=conference_vo_id)
+        return JsonResponse(
+        {"attendees":attendees},
+        encoder = AttendeeListEncoder,
+    )
     else:
-        attendees = Attendee.objects.all()
+        content = json.loads(request.body)
 
         # Get the Conference object and put it in the content dict
-        try:
+    try:
             conference_href = content["conference"]
             conference = ConferenceVO.objects.get(import_href=conference_href)
             content["conference"] = conference
-        except ConferenceVO.DoesNotExist:
-            return JsonResponse(
-                {"message": "Invalid conference id"},
-                status=400,
+    except ConferenceVO.DoesNotExist:
+        return JsonResponse(
+            {"message": "Invalid conference id"},
+            status=400,
             )
 
-        attendee = Attendee.objects.create(**content)
-        return JsonResponse(
-            attendee,
-            encoder=AttendeeDetailEncoder,
-            safe=False,
-        )
+    attendee = Attendee.objects.create(**content)
+    return JsonResponse(
+        attendee,
+        encoder=AttendeeDetailEncoder,
+        safe=False,
+    )
 
 
 def api_show_attendee(request, pk):

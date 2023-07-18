a599c0e76e5455ab6920396cea7996450bca2bb5
leahnp613 <leahnagarpowers@gmail.com>
2023-07-18 15:54:40 -0400

    added routes for the browser router

--- ghi/app/src/App.jsx
+++ ghi/app/src/App.jsx
@@ -3,22 +3,39 @@ import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
 import AttendConferenceForm from './AttendConferenceForm';
 import ConferenceForm from './ConferenceForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
-  return (
-    <>
+  return(   
+    <BrowserRouter>
       <Nav />
       <div className="container">
+        <Routes>
+          <Route path="locations">
+              <Route path="new" element={<LocationForm />}/>
+              </Route>
+          <Route path="conferences">
+                <Route path="new" element={<ConferenceForm />} />
+                </Route>
+          <Route path="attendees">
+                  <Route path="new" element={<AttendConferenceForm />} />
+                  </Route>
+          <Route path="attendees">
+                    <Route path="" element={<AttendeesList />} />
+                    </Route>
         {/* <LocationForm/> */}
         {/* <AttendeesList attendees={props.attendees} /> */}
         {/* {<AttendConferenceForm/>} */}
-        {<ConferenceForm/>}
+        {/* {<ConferenceForm/>} */}
+        </Routes>
       </div>
-    </>
-  );
+    </BrowserRouter>
+   
+    );
 }
 
 export default App;
